7fcdb1ba9f6b87225a09e37770877524047da572
Pedini Jayashree <134771316+pedinistar@users.noreply.github.com>
2025-11-23 10:32:47 +0530

    TC010 - create a post without title - invalid post and verify

--- tests/post-requests.spec.ts
+++ tests/post-requests.spec.ts
@@ -4,7 +4,7 @@ const BASE_URL = 'https://jsonplaceholder.typicode.com'
 
 test.describe("POST API Tests", () => {
     // TC009 - Create new post - verify 201 status
-    test("create new post with valid data and verify 201", async ({ request }) => {
+    test("create new post with valid data and verify 201 and response body", async ({ request }) => {
         const newPost = {
             userId: 1,
             title: 'The new post',
@@ -25,9 +25,20 @@ test.describe("POST API Tests", () => {
         expect(responseBody.id).toBeDefined()
     })
 
+    // TC010 - Create post without title - verify 400 / validation
+    test("create post without title", async ({ request }) => {
+        const invalidPost = {
+            userId: 1,
+            body: "this is body without a title"
+        }
 
-    // TC010 - Create post with valid data - verify response body
-    // TC011 - Create post without title - verify 400 / validation
+        const response = await request.post(`${BASE_URL}/posts`, {
+            data: invalidPost
+        })
+
+        expect(response.status()).toBe(201)
+    })
+
     // TC012 - Create post with special characters - verify handling
     // TC013 - Verify created post has ID
     // TC014 - Verify Location header in response
